6a28e9ce1c29607fb7bb48bb049508e5b0a9ba15
Jorge-Rmz <jorgre5675420j@gmail.com>
2025-06-15 22:03:34 -0600

    se agrega validacion para crear un nuevo projecto y middleware de validacion

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,16 @@
 import {Router} from 'express';
 import { ProjectController } from '../controllers/ProjectController';
+import { body } from 'express-validator';
+import { handleInputErors } from '../middleware/validation';
 
 const router = Router();
 
-router.post('/', ProjectController.createProject)
+router.post('/', 
+    body('projectName').notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clienteName').notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description').notEmpty().withMessage('La descripción del proyecto es obligatoria'),
+    handleInputErors,
+    ProjectController.createProject)
 router.get('/', ProjectController.getAllProjects)
 
 export default router;
